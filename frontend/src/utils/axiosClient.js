5a1b0bf308c0ccfce362e2d0dea5565247b075b4
wizardhappy <142380854+happy0945@users.noreply.github.com>
2026-07-27 14:04:33 +0530

    Change axios base URL to production environment

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://codingplatefrom.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
